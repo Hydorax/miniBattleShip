e5c101850a6daf4d52110a067ec2852e53815340
Sebastian Matyszewski <sebastian@Sebastians-iMac.local>
2020-06-06 16:42:10 +0200

    Dodanie dalszej części rozgrywki

--- miniBattleShip/ViewController.swift
+++ miniBattleShip/ViewController.swift
@@ -18,8 +18,15 @@ class ViewController: UIViewController {
     var ships = 0
     var ownBoard = [0,0,0,0,0,0,0,0,0,0]
     var enemyBoard = [0,0,0,0,0,0,0,0,0,0]
+    var numbers = [1,2,3,4,5,6,7,8,9]
+    var countShipShootedByMe = 0
+    var countShipShootedByEnemy = 0
+    var countShipShooted = 0
+    var percentageShipShooted:Float = 0
     
-    func losuj(n:Int,max:Int){
+    
+    
+    func losujRozstawienieStatkow(n:Int,max:Int){
         
         var wylosowane = [Int]()
         while wylosowane.count<n{
@@ -37,7 +44,7 @@ class ViewController: UIViewController {
     
     @IBAction func buttonPutShips(_ sender: Any) {
         status = 1
-        losuj(n:3, max:9)
+        losujRozstawienieStatkow(n:3, max:9)
         buttonPutShips.isHidden = true
         textPutShips.isHidden = true
         boardEnemy.isHidden = false
@@ -107,8 +114,112 @@ class ViewController: UIViewController {
                 
             
         }else if status == 1{
+            
+            let temp = Int(arc4random_uniform(UInt32(numbers.count)))
+            
+            let number = numbers[temp]
+            numbers.remove(at: temp)
+            //let max = UInt32(9)
+            //let min = UInt32(1)
+                        ////////////////////
+            
+            
+            
+           // repeat{
+               // let index = arc4random_uniform(9)
+                
+                
+                    //if numbers.contains(Int(index)){
+                      //  temp = 0
+                    //}else{
+                       // if Int(index) != 0{
+                            //numbers.append(Int(index))
+                           // liczba = Int(index)
+                            //temp = 1
+                        //}
+                        
+                    //}
+                
+            //}while (temp == 1)
+            
+            //let index = arc4random_uniform(max)
+                //var index2 = Int(index)
+            //while (numbers.contains(Int(index))){
+            //    var index = arc4random_uniform(max)
+            //}
+            
+           // if numbers.isEmpty {
+             //  numbers = Array(1 ... 9)
+           // }
+            //let index = Int(arc4random_uniform(UInt32(numbers.count)))
+            //numbers.remove(at: index)
+            //print("wylosowana:", index)
+            
+            
+            
+            
+            ////////////////////
+            
             a = sender.tag
+            let aa = a - 10
             print(a)
+            print(" ")
+            //let getRandom = losujPolozenieStatkow(min: 1, max: 9)
+            print("Rand", number)
+            //let tmp = Int(getRandom)
+            if enemyBoard[aa] == 1{
+                let tempButton = self.view.viewWithTag(Int(a)) as? UIButton
+                tempButton?.backgroundColor = UIColor.red.withAlphaComponent(0.6)
+                countShipShootedByMe = countShipShootedByMe + 1
+                tempButton?.isEnabled = false
+                countShipShooted = countShipShooted + 1
+                if countShipShootedByMe == 3{
+                    
+                    for n in 1...19{
+                        let tempButton = self.view.viewWithTag(Int(n)) as? UIButton
+                        tempButton?.isEnabled = false
+                        
+                    }
+                    percentageShipShooted = ((Float(countShipShootedByMe)/Float(countShipShooted))*100)
+                    //percentageShipShooted = (4/6)*100
+                    print("Oddane strzały:", countShipShooted)
+                    print("Celne strzały:", countShipShootedByMe)
+                    print("% celności strzałów:", percentageShipShooted)
+                    
+                }
+                
+            }else{
+                let tempButton = self.view.viewWithTag(Int(a)) as? UIButton
+                tempButton?.backgroundColor = UIColor.gray.withAlphaComponent(0.6)
+                tempButton?.isEnabled = false
+                countShipShooted = countShipShooted + 1
+            }
+            sleep(1)
+            if ownBoard[number] == 1{
+                let tempButton = self.view.viewWithTag(number) as? UIButton
+                tempButton?.backgroundColor = UIColor.red.withAlphaComponent(0.6)
+                countShipShootedByEnemy = countShipShootedByEnemy + 1
+                tempButton?.isEnabled = false
+                if countShipShootedByEnemy == 3{
+                    for n in 1...19{
+                        let tempButton = self.view.viewWithTag(Int(n)) as? UIButton
+                        tempButton?.isEnabled = false
+                        
+                    }
+                    //percentageShipShooted = (Float((countShipShootedByMe/countShipShooted)*100))
+                    percentageShipShooted = ((Float(countShipShootedByMe)/Float(countShipShooted))*100)
+                    //percentageShipShooted = (4/6)*100
+                    print("Oddane strzały:", countShipShooted)
+                    print("Celne strzały:", countShipShootedByMe)
+                    print("% celności strzałów:", percentageShipShooted)
+                }
+            }else{
+                let tempButton = self.view.viewWithTag(number) as? UIButton
+                tempButton?.backgroundColor = UIColor.gray.withAlphaComponent(0.6)
+                tempButton?.isEnabled = false
+            }
+            
+            
         }
         
         //let number = Int(arc4random_uniform(9))
